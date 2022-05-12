1f2c4ec58bbdb8f9c070e9369a6fda32e54b1e97
adlina1 <adrien28210@gmail.com>
2022-05-12 17:47:34 +0200

    made sign out and started logics with a navigation drawer

--- app/src/main/java/com/example/immoloc/HomeActivity.java
+++ app/src/main/java/com/example/immoloc/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.immoloc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,11 +9,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
     public TextView search;
@@ -22,11 +27,19 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setNavigationViewListener();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         drawer = findViewById(R.id.drawer_layout);
+
+        // Rendre les items cliquables
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
@@ -44,6 +57,28 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+      if (getIntent().getBooleanExtra("EXIT", false)) {
+            finish();
+        }
+
+      navigationView.bringToFront();
+      navigationView.setNavigationItemSelectedListener(item -> {
+          switch (item.getItemId()) {
+
+              case R.id.nav_logout: {
+                  Intent intent = new Intent(this, MainActivity.class);
+                  intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                  intent.putExtra("EXIT", true);
+                  startActivity(intent);
+                  Toast.makeText(this, "Vous êtes maintenant déconnecté", Toast.LENGTH_SHORT).show();
+                  return true;
+              }
+              case R.id.nav_msg:
+                  Toast.makeText(this, "Vous n'avez pas de messages", Toast.LENGTH_SHORT).show();
+                  return true;
+          }
+          return true;
+      });
     }
 
     @Override
@@ -51,7 +86,37 @@ public class HomeActivity extends AppCompatActivity {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            super.onBackPressed(); // ferme l'activité quand click sur retour
         }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d("logoutcheck", "click done");
+
+        switch (item.getItemId()) {
+
+            case R.id.nav_logout: {
+                Log.d("logoutcheck", "click done");
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                return true;
+            }
+            case R.id.nav_msg:
+                Log.d("logoutcheck", "nav msg  ");
+                Toast.makeText(this, "Clicked item one", Toast.LENGTH_SHORT).show();
+        }
+        //close navigation drawer
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
 }
